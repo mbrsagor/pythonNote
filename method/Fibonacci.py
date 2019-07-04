83836dc3a0ffa3bdd2a190bb78791f6efab5c085
sagor09 <mbrsagor@gmail.com>
2019-07-04 23:18:30 +0600

    Fibonacci number in python

---
+++ method/Fibonacci.py
@@ -0,0 +1,16 @@
+def fib(_start):
+    first = 0
+    last = 1
+
+    if _start == 1:
+        print(first)
+    else:
+        print(first)
+        for start in range(_start):
+            result = first + last
+            first = last
+            last = result
+            print(result)
+
+
+fib(7)
