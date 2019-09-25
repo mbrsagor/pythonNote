990984b52eb19bb7bbb090dcd53a0ab59c094b0b
sagor09 <mbrsagor@gmail.com>
2019-09-25 15:47:13 +0600

    Dictionary and basic python study

--- basic/data-structures/dict/dictionary.py
+++ basic/data-structures/dict/dictionary.py
@@ -18,3 +18,18 @@ for item in items.items():
 print(f"My phone color is: {items.get('Color')}")
 print(items)
 del items['Phone']
+
+aliens = []
+
+for alien_number in range(0, 30):
+    new_alien = {"color": "red", "model": 560}
+    aliens.append(new_alien)
+
+for alien in aliens:
+    if alien['color'] == 'Green':
+        alien['color'] = 'Blue'
+        alien['color'] = 'Orange'
+        alien['color'] = 'Yellow'
+
+for alien in aliens[0:3]:
+      print(alien)
---
+++ basic/introduceSyntax/mosh.py
@@ -0,0 +1,43 @@
+"""
+name = input("Enter your name: ")
+color = input("Enter your favourite color: ")
+
+print(f"{name} likes {color}")
+
+birth_day = int(input("Enter your birthday? "))
+current_date = 2019 - birth_day
+print(current_date)
+
+address = 'House #96 Road#19 "samsu" and Dhaka-1208'
+print(address)
+
+
+about_me = '''I am Sagor.
+Come form Gaibandha.
+Software developer.
+'''
+print(about_me)
+
+
+p = "Python popular web framework is Django "
+print(p[0: 3])
+_p = p.split()
+print(_p)
+
+
+
+N = int(input())
+if N % 2 == 0:
+    print("Odd")
+elif N:
+    print("odd")
+else:
+    print("Event")
+"""
+
+
+
+
+
+
+
