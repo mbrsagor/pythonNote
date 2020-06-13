e03c9187a2c76be2f963591c5fc02cf8d40dd652
mbrsagor <mbrsagor@gmail.com>
2020-06-14 00:30:47 +0600

    Event Odd number findout using function and while loop

---
+++ method/eventOdd.py
@@ -0,0 +1,16 @@
+def is_event(number):
+	if number % 2 == 0:
+		return True
+	else:
+		return False
+
+start_number = 0
+
+while start_number < 100:
+	if is_event(start_number):
+		print(f"{start_number} is event")
+	else:
+		print(f"{start_number} is Odd")
+	start_number = start_number+1
+
+print("The End")
