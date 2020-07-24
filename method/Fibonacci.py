7f1d27fc1d1f8d4e150be70bc8f534a752fe3712
Sagor <mbrsagor@gmail.com>
2020-07-24 12:37:30 +0600

    Fibonacci number findout well way

--- method/Fibonacci.py
+++ method/Fibonacci.py
@@ -14,3 +14,16 @@ def fib(_start):
 
 
 fib(10)
+
+print("\n")
+
+
+def fibo(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+        print()
+
+
+fibo(10)
--- method/lambda.py
+++ method/lambda.py
@@ -1,15 +1,15 @@
 # How to calculate two number
-add = lambda num, num2: num+num2
+add = lambda num, num2: num + num2
 print(add(30, 20))
 
 
 # lambda function using custom function
 def calculate_number(n):
-	return lambda num : n * num
+    return lambda num: n * num
+
 
 cn = calculate_number(5)
 print(cn(5))
-
 
 make_full_name = lambda first_name, last_name: f"Full Name: {first_name} {last_name}"
 
