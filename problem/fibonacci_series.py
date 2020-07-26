f4e79c574369acb7ee2a418a87546265cc54f067
Sagor <mbrsagor@gmail.com>
2020-07-26 18:25:13 +0600

    fibonacci series well formateing

--- problem/fibonacci_series.py
+++ problem/fibonacci_series.py
@@ -1,9 +1,9 @@
 def fibo(n):
+    if n == 1 or n == 2:
+        return n
+    else:
+        return fibo(n - 1) + (n + 1)
 
-  if n == 1 or n == 2:
-    return n
-  else:
-    return fibo(n - 1) + (n + 1)
 
 f = fibo(2)
 print(f)
