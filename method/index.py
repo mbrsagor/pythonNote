a8b0b8e86467f99e86623c9949e6e91dcdf0838a
Sagor <mbrsagor@gmail.com>
2020-11-26 22:43:09 +0600

    One line fuction print

--- method/index.py
+++ method/index.py
@@ -1,10 +1,3 @@
-def say_hello(self):
-    print(f"Hello {self}")
-
-
-say_hello("sagor")
-
-
 def main_function():
     def inner_func():
         print("I am learning python from online.")
@@ -13,3 +6,7 @@ def main_function():
 
 
 main_function()
+
+
+def something(name, profession): return f"Hello this is {name} I'm a {profession}"
+print(something("Sagor", "full-stack software engineer."))
