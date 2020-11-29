07141d0e49b2c4a63d5c83e53a6d1f0089a6915e
Sagor <mbrsagor@gmail.com>
2020-11-29 09:41:37 +0600

    Single link-list and ternary operator basic concept

--- data_structure/tarnary.py
+++ data_structure/tarnary.py
@@ -1,17 +1,16 @@
 first_name = "sagor"
 last_name = "sagor"
-
 result = "Yes!! correct name" if first_name == last_name else "incorrect name"
 print(result)
 
-# tarnary_operator = [(x,y) for x in range(5) for y in range(5) if x * y<10]
-# print(tarnary_operator)
-
-number = True
-output = 10 if number else 0
+person1 = "sagor"
+person2 = "ohi"
+output = "They are equal" if person1 == person2 else "They are not equal"
 print(output)
 
-num1 = 1_000_000
-num2 = 10_000_000
-total = num1+num2
-print(f"{total:,}")
+tarnary_operator = [(x,y) for x in range(5) for y in range(5) if x * y<10]
+print(tarnary_operator)
+
+def checkBigNumber(num, num2): "Num is gettr then num2" if num > num2 else "Invalid request"
+big_num = checkBigNumber(10, 15)
+print(big_num)
