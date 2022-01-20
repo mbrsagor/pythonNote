fb42d80c43dab78d6b2daf3b16a54f8bc57daa29
mbrsagor <mbrsagor@gmail.com>
2022-01-20 23:30:51 +0600

    Refactor: File structure well structure

--- PythonMySql/index.py
+++ PythonMySql/index.py
@@ -1,1 +1,9 @@
-import mysql.connectormydb = mysql.connector.connect(  host="localhost",  user="root",  password=12345678)print(mydb)
+import mysql.connector
+
+db_string = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=12345678
+)
+
+print(db_string)
