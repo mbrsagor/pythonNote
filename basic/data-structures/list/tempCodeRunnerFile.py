e54d3e85b603ee9b7c976128b4481d21d6385fd5
mbrsagor <mbrsagor@gmail.com>
2022-08-11 00:56:00 +0600

    New: simple example of tem cone runfer file.

--- basic/data-structures/list/tempCodeRunnerFile.py
+++ basic/data-structures/list/tempCodeRunnerFile.py
@@ -10,5 +10,29 @@ print(sorted_brand)
 brand.sort()
 print(brand)
 
-
 # Here `.sort` and `sorted` are same work
+
+ages = [5, 12, 17, 18, 24, 32]
+
+
+def function(age):
+    if age < 18:
+        return False
+    else:
+        return True
+
+
+adults = filter(function, ages)
+print(adults)
+for adult in adults:
+    print(adult)
+
+
+def apply_discount(product, discount):
+    price = int(product['price'] * (1.0 - discount))
+    assert 0 <= price <= product['price']
+    return price
+
+
+shoes = {'name': 'Fancy Shoes', 'price': 14900}
+print(apply_discount(shoes, 0.25))
