b2f2fdf7738f14a432c8ea9d1d5270c5a0acc4d2
mbrsagor <mbrsagor@gmail.com>
2022-08-22 02:23:21 +0600

    New: example of comprehension

---
+++ basic/data-structures/list/lc.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+display = [fruit for fruit in fruits if "a" in fruit]
+# display = [fruit for fruit in fruits]
+print(display)
+
+for fruit in fruits:
+    print(fruit)
