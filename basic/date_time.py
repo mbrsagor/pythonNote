f99ebf8893fa7299de19887605abc90e1c8b32b4
mbrsagor <mbrsagor@gmail.com>
2022-11-07 19:55:01 +0600

    New: Worker dashboard API add total duration.

--- basic/date_time.py
+++ basic/date_time.py
@@ -9,3 +9,19 @@ def time_conversion(sec):
 
 
 time_conversion(1667663255)
+
+
+print()
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+
+c = convert(1667663255)
+print(c)
