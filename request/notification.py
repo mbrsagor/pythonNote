b31623f542790bb3882a99b2ca54640efb03c278
mbrsagor <mbrsagor@gmail.com>
2022-11-09 20:03:13 +0600

    New: add push notification from onesingle.

--- request/notification.py
+++ request/notification.py
@@ -2,34 +2,35 @@
 
 import requests
 
-url = "https://onesignal.com/api/v1/apps"
+url = "https://onesignal.com/api/v1/notifications/"
+
+API_KEY = ""
+APP_ID = ""
+authorization = ""
 
 payload = {
-    "name": "NAME_OF_NEW_APP",
-    "apns_env": "ENVIRONMENT",
-    "apns_p12": "CONVERTED_P12",
-    "apns_p12_password": "P12_PASSWORD",
-    "gcm_key": "FCM_KEY",
-    "android_gcm_sender_id": "FCM_ID",
-    "chrome_web_origin": "URL",
-    "chrome_web_default_notification_icon": "DEFAULT_ICON",
-    "chrome_web_sub_domain": "SUBDOMAIN",
-    "chrome_key": "AUTH_KEY",
-    "site_name": "SITE_NAME",
-    "safari_site_origin": "URL",
-    "safari_apns_p12": "CONVERTED_P12",
-    "safari_apns_p12_password": "P12_PASSWORD",
-    "safari_icon_256_256": "ICON_URL",
-    "additional_data_is_root_payload": False,
-    "organization_id": "ORG_ID"
+    "device_type": 0,
+    "language": "en",
 }
+
+body = {
+    "app_id": APP_ID,
+    "app_name": "Hercules",
+    "included_segments": ["Subscribed Users"],
+    # "include_player_ids": ["8b2df38d-62cc-48a9-bad0-407bdc80b820"],
+    "android_sound": "notification",
+    "large_icon": "https://res.cloudinary.com/mbrsagor/image/upload/v1640713048/booking_bg_wzssss.jpg",
+    "big_picture": "https://res.cloudinary.com/mbrsagor/image/upload/v1640713048/booking_bg_wzssss.jpg",
+    # "data": {"image_url": "https://i.picsum.photos/id/237/200/300.jpg"},
+    "headings": {"en": "Hello, delivery Mama"},
+    "contents": {"en": 'Welcome Hercules App!'},
+}
+
 headers = {
     "accept": "text/plain",
     "Content-Type": "application/json",
-    "Authorization": "Basic YOUR_USER_AUTH_KEY"
+    "Authorization": f"Basic {authorization}"
 }
 
-response = requests.post(url, json=payload, headers=headers)
-
+response = requests.post(url, json=body, headers=headers)
 print(response.text)
-
