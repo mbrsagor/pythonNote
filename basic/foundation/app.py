1d2cac770ab210dfdf8ba5c64caf1d236ded3874
mbrsagor <mbrsagor@gmail.com>
2023-03-23 20:07:34 +0600

    New: project details API create.

--- basic/foundation/app.py
+++ basic/foundation/app.py
@@ -1,26 +1,4 @@
-"""
-first_name = "mbr"
-last_name = "sagor"
 
-message = first_name+"("+last_name+")"+"is a coder"
-print(message)
-
-
-mes = f'{first_name} ({last_name}) is a coder'
-print(mes)
-
-
-print(len(message))
-print(message.upper())
-
-print(message.find('d'))
-
-name = "mbr sagor"
-print(name.replace('mbr', 'Md'))
-
-print('sagor' in name)
-
-"""
 
 li = [20, 40, 10, 30]
 total = 0
