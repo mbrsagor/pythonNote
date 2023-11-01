5e07e4bfd2dd2de44d2c033171ae91da1de4d8bd
mbrsagor <mbrsagor@gmail.com>
2023-11-01 21:34:57 +0600

    New: create instance using python both3.

--- file/s3fileupload.py
+++ file/s3fileupload.py
@@ -6,12 +6,13 @@ s3.upload_file('/tmp/hello.txt', 'mybucket', 'hello.txt')
 pip3 install boto3
 """
 
+# Another Way
+
 REGION = 'us-east-1' 
 ACCESS_KEY_ID = 'paste_here_your_key_id' 
 SECRET_ACCESS_KEY = 'paste_here_your_secret_access_key' PATH_IN_COMPUTER = 'path/in/computer/namefile.txt' 
 BUCKET_NAME = 'vperezb' 
 KEY = 'path/in/s3/namefile.txt' # file path in S3 
-
 
 s3_resource = boto3.resource(
     's3', 
