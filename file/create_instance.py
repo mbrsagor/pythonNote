3148d37c7cdda0ae67494a1d236a00bcb20ea5d3
mbrsagor <mbrsagor@gmail.com>
2023-11-01 21:38:23 +0600

    New: Create an EC2 Instance using Python Boto3.

--- file/create_instance.py
+++ file/create_instance.py
@@ -10,3 +10,30 @@ instances = ec2.create_instances(
     )
 
 
+client = boto3.client('ec2', region_name='us-west-2')
+
+response = client.run_instances(
+    BlockDeviceMappings=[
+        {
+            'DeviceName': '/dev/xvda',
+            'Ebs': {
+
+                'DeleteOnTermination': True,
+                'VolumeSize': 8,
+                'VolumeType': 'gp2'
+            },
+        },
+    ],
+    ImageId='ami-6cd6f714',
+    InstanceType='t3.micro',
+    MaxCount=1,
+    MinCount=1,
+    Monitoring={
+        'Enabled': False
+    },
+    SecurityGroupIds=[
+        'sg-1f39854x',
+    ],
+)
+
+
