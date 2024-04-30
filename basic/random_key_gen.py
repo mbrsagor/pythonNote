b7e2e1be97bb3c6d5f7dc87feaa10b834d3156f4
mbrsagor <mbrsagor@gmail.com>
2024-04-30 10:34:38 +0600

    New: Random key generator

--- basic/random_key_gen.py
+++ basic/random_key_gen.py
@@ -1,7 +1,12 @@
 import string
 import secrets
 
-alphabet = string.ascii_letters + string.digits
-key = ''.join(secrets.choice(alphabet) for i in range(12))
 
-print(key.upper())
+def random_key_generator(limit):
+    alphabet = string.ascii_letters + string.digits
+    key = ''.join(secrets.choice(alphabet) for i in range(limit))
+    return key.upper()
+
+
+if __name__ == '__main__':
+    print(random_key_generator(50))
