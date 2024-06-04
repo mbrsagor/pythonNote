fd0079b548abccabfabd3a37d9291804ad938624
mbrsagor <mbrsagor@gmail.com>
2024-06-04 22:59:40 +0600

    New: Simple example file zip of python.

--- extra/file_manager_srp.py
+++ extra/file_manager_srp.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from zipfile import ZipFile
 
 
-class FileManager:
+class FileManager(object):
     def __init__(self, filename):
         self.path = Path(filename)
 
@@ -19,3 +19,8 @@ class FileManager:
     def decompress(self):
         with ZipFile(self.path.with_suffix(".zip"), mode="r") as archive:
             archive.extractall()
+
+
+if __name__ == "__main__":
+    fm = FileManager('README.md')
+    print(fm.read())
