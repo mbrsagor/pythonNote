627163241112241dfcd2e308f11e623bc6c08b91
mbrsagor <mbrsagor@gmail.com>
2024-06-06 19:03:33 +0600

    New: Simple AWS lambda example.

--- extra/lambda.py
+++ extra/lambda.py
@@ -2,7 +2,17 @@ import boto3
 import botocore
 
 
+# def lambda_handler(event, context):
+#    print(f'boto3 version: {boto3.__version__}')
+#    print(f'botocore version: {botocore.__version__}')
+
+
+import json
+
 def lambda_handler(event, context):
-   print(f'boto3 version: {boto3.__version__}')
-   print(f'botocore version: {botocore.__version__}')
+    print("Hello from Lambda!")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
 
