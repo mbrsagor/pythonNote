dba1f4bbf97ce1a0cfb0457b1a79a2aa94759c39
mbrsagor <mbrsagor@gmail.com>
2024-07-31 10:14:18 +0600

    Refactor: job problem3 refactor.

--- basic/problem3.py
+++ basic/problem3.py
@@ -1,14 +1,17 @@
-def display(L, negative=True, odd=True):
-  for n in L:
-    if n < 0 and negative is False:
-      continue
-    if n % 2 != 0 and odd is False:
-      continue
-    print(n, end='')
-  print()
+def display(L, nagative=True, odd=True):
+    for n in L:
+        if n < 0 and nagative is False:
+            continue
+        if n % 2 !=0 and odd is False:
+            continue
+        print(n, end=' ')
+    print()
+    
 
+numbers = [12, -1, 3, 6, 8, 9, 38, -3, 34, -4]
 
-numbers = [12, -1, 3, 6, 8, 9, 38, -3, 34, -4)
 display(numbers, True, False)
 display(numbers, False)
-display(numbers, False, True)
+display(numbers, True, odd=False)
+display(numbers, True, nagative=False)
+display(numbers, nagative=False, odd=False)
