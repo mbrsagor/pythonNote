c995fb5e0dc8f2589edfeabb5cc8ac02e8e592aa
mbrsagor <mbrsagor@gmail.com>
2024-08-05 10:58:56 +0600

    Refactor: Python problem3 spling correct.

--- basic/problem3.py
+++ basic/problem3.py
@@ -1,6 +1,6 @@
-def display(L, nagative=True, odd=True):
+def display(L, negative=True, odd=True):
     for n in L:
-        if n < 0 and nagative is False:
+        if n < 0 and negative is False:
             continue
         if n % 2 !=0 and odd is False:
             continue
@@ -13,5 +13,5 @@ numbers = [12, -1, 3, 6, 8, 9, 38, -3, 34, -4]
 display(numbers, True, False)
 display(numbers, False)
 display(numbers, True, odd=False)
-display(numbers, True, nagative=False)
-display(numbers, nagative=False, odd=False)
+display(numbers, True, negative=False)
+display(numbers, negative=False, odd=False)
