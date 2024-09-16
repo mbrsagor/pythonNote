ffe30d322901f2c641d949517a390c3a295bd623
mbrsagor <mbrsagor@gmail.com>
2024-09-16 11:05:51 +0600

    New: Random integer key geneate.

--- basic/random_key_gen.py
+++ basic/random_key_gen.py
@@ -1,11 +1,18 @@
 import string
 import secrets
-
+import random
 
 def random_key_generator(limit):
     alphabet = string.ascii_letters + string.digits
     key = ''.join(secrets.choice(alphabet) for i in range(limit))
     return key.upper()
+
+
+random_key = random.randint(1000, 9999)
+print("Generated random key:", random_key)
+
+random_key = random.getrandbits(128)
+print("Generated random 128-bit key:", random_key)
 
 
 if __name__ == '__main__':
