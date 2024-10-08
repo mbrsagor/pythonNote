c55e3aa03b62b8a781208f4c96f71d037a4bada9
mbrsagor <mbrsagor@gmail.com>
2024-10-08 23:46:52 +0600

    New: profile update done.

--- basic/foundation/wfc.py
+++ basic/foundation/wfc.py
@@ -4,3 +4,9 @@ URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
 print(page.text)
+
+# Convert tuple to integer
+qtn = ('3', )
+_qtn = int(''.join(map(str, qtn)))
+print(f"Final result: {_qtn}")
+
